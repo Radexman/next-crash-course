f95be190aeca106562d91c82cc878424fe092ef5
Radexman <Borderlandsmaniak@gmail.com>
2025-02-25 22:37:32 +0100

    Paragraph

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -4,6 +4,11 @@ const layout = ({ children }: { children: ReactNode }) => {
   return (
     <div>
       <h1 className="text-3xl">Dashboard</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque omnis
+        corrupti earum numquam quis dicta nulla, nemo at in, quos adipisci
+        corporis, beatae possimus magni.
+      </p>
       {children}
     </div>
   );
